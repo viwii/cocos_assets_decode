3d30b13f3d9cf69f81527621c48ebc0a74789d31
viwii <viwii@sina.cn>
2019-07-26 23:00:30 +0800

    添加资源

---
+++ chidouren/assets/Script/SoundButton.js
@@ -0,0 +1,11 @@
+ cc.Class({
+    extends: cc.Component,
+    properties: {},
+    start: function() {},
+    click: function() {
+        ss.logic.sound.btnClick();
+    },
+    close: function() {
+        ss.logic.sound.btnClick();
+    }
+})
